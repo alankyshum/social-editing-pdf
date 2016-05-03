f1c2443e36ffdc1b791b35a4158e82439cd20e64
Alan Shum <alan200994@gmail.com>
2016-05-03 18:29:07 +0800

    added old browser support

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('build:scss', function () {
   return gulp.src('scss/**/*.scss')
   .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions']
+  }))
   .pipe(gulp.dest('css'));
 });
 
